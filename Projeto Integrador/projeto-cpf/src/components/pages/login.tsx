7ed2892a55065da5577ca43fdaba2e7e8519d0f9
Jose <jose.weverton.uni@gmail.com>
2023-05-02 22:19:14 -0300

    Atualizando

--- Projeto Integrador/projeto-cpf/src/components/pages/login.tsx
+++ Projeto Integrador/projeto-cpf/src/components/pages/login.tsx
@@ -13,8 +13,13 @@ function Login() {
             
             <div className="container_login">
 
+                <header>
+                    <h1><a href="http://">Cadastre-se aqui!</a></h1>
+                </header>
+
                 <input type="text" className='input-login' placeholder='Login' /> 
                 <input type="password" className='input-login' placeholder='Password' />
+                <input type="submit" className='login-acessar' value="Acessar" />
 
             </div>
         </div>
