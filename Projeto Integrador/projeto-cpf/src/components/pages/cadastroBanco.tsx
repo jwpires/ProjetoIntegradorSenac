90119f0fa398e6c04a6c767e8832c5a63ef1e838
Jose <jose.weverton.uni@gmail.com>
2023-05-08 23:26:50 -0300

    atualizando

--- Projeto Integrador/projeto-cpf/src/components/pages/cadastroBanco.tsx
+++ Projeto Integrador/projeto-cpf/src/components/pages/cadastroBanco.tsx
@@ -3,30 +3,44 @@ import HeaderMenu from '../header/HeaderMenu';
 
 function CadastroBanco() {
 
-    return(
-        
-        <div className="container-cadBanco">
-            <HeaderMenu exibe={true}/>
-
-            <div className="cadBanco">
-                <form action="" method="post">
-                    <div>
-                        <select name="Banco" id="TESTE">
-                            <option value="">Informe o Banco</option>
-                        </select>
-                        <button type='button'> + </button> {/* Botão servirá para direcionar a tela de cadastro da conta bancária. */}
-                    </div>
-                    
-                    <input type="text" name="" id="" placeholder='Nome do proprietário da conta:'/>
-                    <input type="text" name="" id="" placeholder='Informe número da conta'/>
-                    
-
-                </form>
+    return (
+        <div className="container_main_padrao">
+                       
+            <div className="container_padrao">
                 
+                <header>
+                    <h1 className='descricao'>Cadastre suas Contas!</h1>
+                </header>
+
+                <div className="cadBanco">
+                    <form action="" method="post">
+                        <div>
+                            <select name="Banco" id="">
+                                <option value="">Informe o Banco</option>
+                            </select>
+                            <button type='button'> + </button> {/* Botão servirá para direcionar a tela de cadastro da conta bancária. */}
+                        </div>
+                        
+                        <input type="text" name="" id="" placeholder='Nome do proprietário da conta:'/>
+                        <input type="text" name="" id="" placeholder='Informe número da conta'/>
+                        
+                        <div className="raio_group">
+                            <label><input type="radio" name="tipo_conta" id="" />Conta Corrente</label>
+                            <label><input type="radio" name="tipo_conta" id="" />Conta Salário</label>
+                            <label><input type="radio" name="tipo_conta" id="" />Conta Poupança</label>
+                            <input type="text" name="" id="" placeholder='Saldo inicial:'/>
+                        </div>
+                    </form>
+                </div>
+
             </div>
-            
-            
+
+            <div className="container_imagem">
+                <img src={require("../../images/logo.png")} alt="exibe imagem do login" />
+                <h1 className='descricao'>Controle Pessoal de Finanças</h1>
+            </div>
         </div>
+        
     );
     
 }
