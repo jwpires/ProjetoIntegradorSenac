914cc4f7534497fc05d845ac67d780dcba58006f
Jose <jose.weverton.uni@gmail.com>
2023-05-24 22:21:03 -0300

    atualizando

--- Projeto Integrador/projeto-cpf/src/components/pages/lancamentoDespesa.tsx
+++ Projeto Integrador/projeto-cpf/src/components/pages/lancamentoDespesa.tsx
@@ -2,7 +2,15 @@ import '../../style/style.css';
 import HeaderMenu from '../header/HeaderMenu';
 import { useNavigate } from 'react-router-dom';
 
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 function LancamentoDespesa(){
+/**--------------------------------------------*/
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+/**--------------------------------------------*/
     const navegacao = useNavigate();
 
     function handleClickGrupoDespesa(){
@@ -27,7 +35,7 @@ function LancamentoDespesa(){
 
                     <div className='divCadBanco'>
                         
-                        <form action="" method="post" className="cadBanco">
+                        <form action=""  className="cadBanco">
                             <input type="text" name="" id="" placeholder='Descrição da Despesa:' />
                             <div className='adiciona-banco'>
                                 <select name="GrupoDespesa" id="">
@@ -48,8 +56,58 @@ function LancamentoDespesa(){
                                 <button type='button' > + </button> {}
                             </div>                                                      */}
                             
-                            <input type="submit" id='salvar' value="salvar" />
-                            <button type="submit" className='botao-padrao' onClick={handleClickConfirmaPagamento}>Pagar</button>
+                            
+                            <button className="botao-padrao">Salvar</button>
+                            
+                            
+                            <input type="button" className='botao-pagar'  onClick={toggle} value="salvar" />
+                            
+                        
+                            <div className="teste" >
+                                
+                                <br />
+                                <button className='botao-pagar' onClick={toggle}>Pagar</button>
+                                
+                                {/* <Button onClick={toggle} >Pagar</Button> */}
+                                <Modal isOpen={modal} toggle={toggle}>
+                                    <ModalHeader toggle={toggle}>Confirmação de pagamento!</ModalHeader>
+                                    <ModalBody>
+                                
+                                                
+                                                <form action="" className="cadBanco">
+                                                    <input type="text" name="" id="" placeholder='Descrição da Despesa:' />
+                                                    
+                                                    <label>Data de Vencimento:</label>
+                                                    <input type="date" name="" id="" />
+                                                    
+                                                    <input type="text" name="" id="" placeholder='Valor da Despesa:'/>
+                                                    
+                                                    <label>Forma de Pagamento:</label>
+                                                    <div className="raio_group">
+
+                                                        <label><input type="radio" name="tipo_pagamento" id="" />Conta Bancária</label><br />
+                                                        <label><input type="radio" name="tipo_pagamento" id="" />Carteira</label><br />
+                                                        
+                                                    </div>
+                                                    <div className='adiciona-banco'>
+                                                            <select name="Banco" id="">
+                                                                <option value="">Informe o Banco</option>
+                                                            </select>
+                                                            <button type='button' > + </button> {/* Botão servirá para direcionar a tela de cadastro da conta bancária. */}
+                                                    </div>
+
+                                                </form>
+                                    </ModalBody>
+                                    <ModalFooter>
+                                        <Button color="primary" onClick={toggle}>
+                                            Confirmar
+                                        </Button>{' '}
+                                        <Button color="secondary" onClick={toggle}>
+                                            Cancelar
+                                        </Button>
+                                    </ModalFooter>
+                                </Modal>
+                            </div>
                         </form>
                     </div>
 
