cdd89f2627de9e6c7bb65dd4de585576cfe04b54
Jose <jose.weverton.uni@gmail.com>
2023-07-08 20:00:34 -0300

    atualizando

--- API/projeto-cpf_api/src/LancamentoDespesa/despesa.dm.ts
+++ API/projeto-cpf_api/src/LancamentoDespesa/despesa.dm.ts
@@ -3,7 +3,6 @@ import { DespesaEntity } from "./despesa.entity";
 
 @Injectable()
 export class DespesasArmazenadas{
-<<<<<<< HEAD
     #despesas: DespesaEntity[] = [];
     
     get Despesa() {
@@ -18,7 +17,4 @@ export class DespesasArmazenadas{
         const despesas = this.Despesa;
         return despesas;
     }
-=======
-    #despesas : DespesaEntity[];
->>>>>>> 09402edd8a19c6c5309fa8db0f3f5617d4c40984
 }
