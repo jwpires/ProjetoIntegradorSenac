8ad0c86b74d7687d51333aeac92f24c8e5c57045
Jose <jose.weverton.uni@gmail.com>
2023-08-24 19:33:42 -0300

    atualizando

--- API/projeto-cpf_api/dist/relatorios/relatorios.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Agencia } from "src/agencia/agencia.entity";
-import { Repository } from "typeorm";
-export declare class RelatorioService {
-    private relatorioSaldoDash;
-    constructor(relatorioSaldoDash: Repository<Agencia>);
-    listar(): Promise<Agencia[]>;
-}
