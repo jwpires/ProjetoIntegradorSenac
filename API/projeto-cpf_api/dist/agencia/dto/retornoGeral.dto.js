58a2a5215ed852ec06821a5443ce407e20c5e4c3
Guilherme Capelli <129558769+GuilhermeCapelli@users.noreply.github.com>
2023-09-12 19:12:35 -0300

    atualizando

--- API/projeto-cpf_api/dist/agencia/dto/retornoGeral.dto.js
+++ API/projeto-cpf_api/dist/agencia/dto/retornoGeral.dto.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RetornoGeralDTO = void 0;
 class RetornoGeralDTO {
-    constructor(id, descricao) {
+    constructor(id, descricao, email, senha) {
         this.id = id;
         this.descricao = descricao;
+        this.email = email;
+        this.senha = senha;
     }
 }
 exports.RetornoGeralDTO = RetornoGeralDTO;
