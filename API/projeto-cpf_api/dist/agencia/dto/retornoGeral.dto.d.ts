255d58e3ad5119078a7f065f61b7717e295a4864
Jose <jose.weverton.uni@gmail.com>
2023-09-12 21:00:15 -0300

    commit exclusão da marca

--- API/projeto-cpf_api/dist/agencia/dto/retornoGeral.dto.d.ts
+++ API/projeto-cpf_api/dist/agencia/dto/retornoGeral.dto.d.ts
@@ -1,7 +1,5 @@
 export declare class RetornoGeralDTO {
     readonly id: string;
     readonly descricao: string;
-    readonly email: string;
-    readonly senha: string;
-    constructor(id: string, descricao: string, email: string, senha: string);
+    constructor(id: string, descricao: string);
 }
--- API/projeto-cpf_api/dist/marcaTesteConexaoBanco/dto/criamarca.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CriaMarcaDTO {
-    nome: string;
-}
